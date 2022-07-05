5ea3e2ae46c302af3495a54c2d74cd043f4a0e32
Cesar <cesarandresgj95@ggmail.com>
2022-07-04 22:52:39 -0400

    Aqui no nos salio

---
+++ src/gui/Formulario.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javax.swing.*;
+
+public class Formulario extends JFrame{
+    private JPanel panel1;
+    private JTextField txtFecha;
+    private JTextField txtNombre;
+    private JTextField txtCorreo;
+    private JButton btnCrear;
+    private JButton btnSalir;
+    private JButton btnLimpiar;
+    private JTextField txtFono;
+    private JLabel jbNom;
+    private JLabel jbCorreo;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        setContentPane(panel1);
+
+
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,5 +1,20 @@
+import gui.Formulario;
+
+import javax.swing.*;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("INICIO DE LA APP");
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame= new Formulario();
+                frame.setVisible(true);
+                frame.setSize(300,300);
+
+            }
+        });
     }
+
+
 }
