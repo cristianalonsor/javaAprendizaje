8a90812e71a4f26783d6ff753d98db3892eb86e8
Cesar <cesarandresgj95@ggmail.com>
2022-07-04 22:16:25 -0400

    Creacion de la clase Usuario

---
+++ src/clases/Usuario.java
@@ -0,0 +1,60 @@
+package clases;
+
+import java.util.Date;
+
+public class Usuario {
+    int idUsuario;
+    int tipoUsuario;
+    String nombre;
+    Date fechaNac;
+    String email;
+    int fono;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(int tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getFono() {
+        return fono;
+    }
+
+    public void setFono(int fono) {
+        this.fono = fono;
+    }
+}
